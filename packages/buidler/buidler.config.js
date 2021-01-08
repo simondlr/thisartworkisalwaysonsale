a48b9320404f6fee3c1948a5a02895366e8d2e47
Simon de la Rouviere <simon@delarouviere.com>
2021-01-08 10:39:05 +0200

    in progress branch commit

--- packages/buidler/buidler.config.js
+++ packages/buidler/buidler.config.js
@@ -1,7 +1,11 @@
 const defaultAccounts = require("./scripts/wallet-utils.js");
-const { usePlugin } = require('@nomiclabs/buidler/config');
-usePlugin("@nomiclabs/buidler-waffle");
-usePlugin("@nomiclabs/buidler-web3");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+// require("@nomiclabs/hardhat-etherscan");
+
+let infuraID = process.env.REACT_APP_INFURA_ID; 
+
+const deployAccounts = defaultAccounts();
 
 module.exports = {
   defaultNetwork: 'localhost',
@@ -9,12 +13,20 @@ module.exports = {
     localhost: {
       url: 'http://localhost:8545',
     },
-    buidlerevm: {
+    hardhat: {
       accounts: defaultAccounts(),
       gas: 9500000,
-    }
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${infuraID}`,
+      accounts: [deployAccounts[0].privateKey],
+      gasPrice: 80e9
+    },
+  },
+  etherscan: {
+    apiKey: ''
   },
   solc: {
-    version : "0.6.6",
+    version : "0.7.6",
   }
 }
