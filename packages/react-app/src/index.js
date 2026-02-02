f6019bcaf1452c3486a3f337815cd0ede35ac9d6
Simon de la Rouviere <simon@delarouviere.com>
2026-02-02 16:41:59 -0500

    remove react-app and re-add favicon

--- packages/react-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import AppRoutes from "./App";
-
-ReactDOM.render(
-    <AppRoutes />,
-  document.getElementById("root"),
-);
